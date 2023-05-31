7f20e65b45cc013001f99a57e34a2766bddd9c30
Mohammed Nasser <134812736+mohamednasser018@users.noreply.github.com>
2023-05-31 20:16:55 +0300

    Update providers.tf

--- basic_network/providers.tf
+++ basic_network/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-    access_key = "AKIATRBJEPDEK4OX6KG7"
-  secret_key = "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"
+    access_key = ""
+  secret_key = ""
 }
